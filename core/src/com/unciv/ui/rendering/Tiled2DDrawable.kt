1409fc674b7c0a77a54a585dc2c45e1ecba3762d
mgongalsky <54280807+mgongalsky@users.noreply.github.com>
2023-07-21 22:56:20 +0300

    Advanced rendering: tiled drawables can be linear

--- core/src/com/unciv/ui/rendering/Tiled2DDrawable.kt
+++ core/src/com/unciv/ui/rendering/Tiled2DDrawable.kt
@@ -6,11 +6,16 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureWrap
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable
 import com.badlogic.gdx.utils.JsonValue
+
+enum class Tilability { PLANE, X, Y}
 
 class Tiled2DDrawable : Drawable {
     //private lateinit var texture: Texture
     private lateinit var textureRegion: TextureRegion
+    private var tilability = Tilability.PLANE
+    private var tilabilityString = ""
 
     constructor() : super() {
         // no-arg constructor for JSON instantiation
@@ -18,30 +23,69 @@ class Tiled2DDrawable : Drawable {
 
     constructor(skin: Skin, jsonData: JsonValue) : this() {
         textureRegion = skin.getRegion(jsonData.getString("textureRegion"))
-        textureRegion.texture.setWrap(TextureWrap.Repeat, TextureWrap.Repeat)
+
+        //tilability = Tilability.valueOf(jsonData.getString("tilability"))
+        tilabilityString = jsonData.getString("tilability")
+
+
+        setTilability()
+
+
     }
 
     constructor(region: TextureRegion) {
         textureRegion = TextureRegion(region)
-        textureRegion.texture.setWrap(TextureWrap.Repeat, TextureWrap.Repeat)
-
     }
 
 
     init {
+        setTilability()
+    }
+
+    private fun setTilability() {
+
+        if (tilabilityString != "")
+            tilability = when (tilabilityString) {
+                "PLANE" -> Tilability.PLANE
+                "X" -> Tilability.X
+                "Y" -> Tilability.Y
+                else -> throw IllegalArgumentException("Invalid tilability value in Skin.json")
+            }
+
     }
 
     override fun draw(batch: Batch, x: Float, y: Float, width: Float, height: Float) {
-        textureRegion.texture.setWrap(TextureWrap.Repeat, TextureWrap.Repeat)
 
+        setTilability()
         val tileWidth = textureRegion.regionWidth.toFloat()
         val tileHeight = textureRegion.regionHeight.toFloat()
 
-        for (drawX in 0 until width.toInt() step tileWidth.toInt()) {
-            for (drawY in 0 until height.toInt() step tileHeight.toInt()) {
-                batch.draw(textureRegion, x + drawX, y + drawY, tileWidth, tileHeight)
-            }
+        when (tilability) {
+            Tilability.PLANE ->
+                for (drawX in 0 until width.toInt() step tileWidth.toInt())
+                    for (drawY in 0 until height.toInt() step tileHeight.toInt()) {
+                        batch.draw(textureRegion, x + drawX, y + drawY, tileWidth, tileHeight)
+                    }
+            Tilability.X ->
+                for (drawX in 0 until width.toInt() step tileWidth.toInt())
+                    batch.draw(textureRegion, x + drawX, y, tileWidth, tileHeight)
+            Tilability.Y ->
+                for (drawY in 0 until height.toInt() step tileHeight.toInt()) {
+                    batch.draw(textureRegion, x, y + drawY, tileWidth, tileHeight)
+                }
         }
+/*
+
+                } == Tilability.PLANE || tilability == Tilability.X)
+            for (drawX in 0 until width.toInt() step tileWidth.toInt()) {
+                if (tilability == Tilability.PLANE || tilability == Tilability.Y)
+                    for (drawY in 0 until height.toInt() step tileHeight.toInt()) {
+                        batch.draw(textureRegion, x + drawX, y + drawY, tileWidth, tileHeight)
+                    }
+            }
+
+
+ */
     }
 
     override fun getLeftWidth() = 0f
