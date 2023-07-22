a6375755998078bb39f07f9173de7162d0030665
mgongalsky <54280807+mgongalsky@users.noreply.github.com>
2023-07-23 01:57:57 +0300

    Get rid of Tiled2DDrawable.kt. However, full TilableFrame.kt still not finished. Mirroring of TiledDrawables does not work.

--- core/src/com/unciv/ui/rendering/TilableFrame.kt
+++ core/src/com/unciv/ui/rendering/TilableFrame.kt
@@ -7,7 +7,10 @@ import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable
+import com.badlogic.gdx.scenes.scene2d.utils.TransformDrawable
 import com.badlogic.gdx.utils.JsonValue
+import kotlin.math.min
 
 class TilableFrame : Drawable{
 
@@ -23,14 +26,20 @@ class TilableFrame : Drawable{
     //  leftBottomDrawable - leftBottomHorizontalDrawable ------ bottomDrawable ------ rightBottomHorizontalDrawable - rightBottomDrawable
 
 
-    var background: Tiled2DDrawable? = null
+    var tiledBackground: TiledDrawable? = null
+
     var leftTopDrawable: Drawable? = null
     var leftTopHorizontalDrawable: Drawable? = null
+    var leftTopVerticalDrawable: Drawable? = null
     var rightTopHorizontalDrawable: Drawable? = null
+    var rightTopVerticalDrawable: Drawable? = null
+
     var rightTopDrawable: Drawable? = null
-    var topDrawable: Tiled2DDrawable? = null
-    var leftDrawable: Tiled2DDrawable? = null
-    var rightDrawable: Tiled2DDrawable? = null
+
+    var tiledTopDrawable: TiledDrawable? = null
+
+    var leftTiledDrawable: TiledDrawable? = null
+    var rightTiledDrawable: TiledDrawable? = null
     // TODO: add here untilable parts or corners (leftTopBorder, etc.)
 
     constructor() : super() {
@@ -41,16 +50,28 @@ class TilableFrame : Drawable{
         //background = Tiled2DDrawable(skin)
         //textureRegion = skin.getRegion(jsonData.getString("textureRegion"))
 
-        background = skin.get(jsonData.getString("background"), Tiled2DDrawable::class.java)
+        tiledBackground = skin.get(jsonData.getString("background"), TiledDrawable::class.java)
+       // tiledBackground = skin.get(jsonData.getString("background"), TiledDrawable::class.java)
         leftTopDrawable = skin.get(jsonData.getString("leftTopDrawable"), Drawable::class.java)
         leftTopHorizontalDrawable = skin.get(jsonData.getString("leftTopHorizontalDrawable"), Drawable::class.java)
-        topDrawable = skin.get(jsonData.getString("topDrawable"), Tiled2DDrawable::class.java)
-        leftDrawable = skin.get(jsonData.getString("leftDrawable"), Tiled2DDrawable::class.java)
-
+        leftTopVerticalDrawable = skin.get(jsonData.getString("leftTopVerticalDrawable"), Drawable::class.java)
+        tiledTopDrawable = skin.get(jsonData.getString("tiledTopDrawable"), TiledDrawable::class.java)
+      //  tiledTopDrawable = skin.get(jsonData.getString("topDrawable"), TiledDrawable::class.java)
+        leftTiledDrawable = skin.get(jsonData.getString("tiledLeftDrawable"), TiledDrawable::class.java)
+        //initTiledDrawables()
 
     }
 
     constructor(region: TextureRegion) {
+    }
+
+    fun initTiledDrawables(){
+        //if(background != null && tiledBackground == null)
+       //     tiledBackground = background as TiledDrawable
+       // if(topDrawable != null && tiledTopDrawable == null)
+       //     tiledTopDrawable = topDrawable as TiledDrawable
+
+
     }
 
 
@@ -60,38 +81,78 @@ class TilableFrame : Drawable{
 
 
     override fun draw(batch: Batch, x: Float, y: Float, width: Float, height: Float) {
-        background?.draw(batch,x,y,width,height)
+        //var textureRegion = background?.textureRegion
+       // initTiledDrawables()
+       // val tiledDrawable = TiledDrawable(textureRegion)
+      //  tiledBackground = background as TiledDrawable
+        //Drawable
 
-        if(leftDrawable != null) {
-            leftDrawable?.draw(
+// Now you can use the TiledDrawable to draw
+// Assume batch is your SpriteBatch, and you want to fill an area at (50,50) with size (200,200)
+        ////batch.begin()
+        //tiledBackground?.draw(batch, x,y,width,height)
+        tiledBackground?.draw(
+            batch,
+            x + leftTopDrawable?.minWidth!! / 2,
+            y + x + leftTopDrawable?.minHeight!! / 2,
+            width - leftTopDrawable?.minWidth!!,
+            height - leftTopDrawable?.minHeight!!
+        )
+        //batch.end()
+
+       // background?.draw(batch,x,y,width,height)
+
+        if(leftTiledDrawable != null) {
+            var spacerUp = 0f
+            if(leftTopDrawable != null)
+                spacerUp += leftTopDrawable?.minHeight!!
+            if(leftTopVerticalDrawable != null)
+                spacerUp += leftTopVerticalDrawable?.minHeight!!
+
+            var spacerDown = spacerUp
+
+            /*
+            tiledTopDrawable?.draw(
+                batch,
+                x + spacerLeft,
+                y + height - tiledTopDrawable?.region?.regionHeight!!.toFloat(),
+                (width - spacerLeft - spacerRight),
+                tiledTopDrawable?.region?.regionHeight!!.toFloat()
+            )
+
+
+             */
+
+            leftTiledDrawable?.draw(
                 batch,
                 x,
-                y,
-                leftDrawable?.minWidth!!,
-                height
+                y + spacerDown,
+                leftTiledDrawable?.region?.regionWidth?.toFloat()!!,
+                height - spacerDown - spacerUp
             )
-            if (rightDrawable == null){
-                leftDrawable?.anchor = Anchor.RIGHT
-                leftDrawable?.draw(
+            if (rightTiledDrawable == null){
+                //leftTiledDrawable?.scale = -1f
+                //leftTiledDrawable.draw()
+                //TransformDrawable
+                leftTiledDrawable?.draw(
                     batch,
-                    x+width-leftDrawable?.minWidth!!,
-                    y,
-                    leftDrawable?.minWidth!!,
-                    height
+                    x + width - leftTiledDrawable?.region?.regionWidth?.toFloat()!!,
+                    y + spacerDown,
+                    leftTiledDrawable?.region?.regionWidth?.toFloat()!!,
+                    height - spacerDown - spacerUp,
                 )
-                leftDrawable?.anchor = Anchor.LEFT
             }
         }
 
         if(leftTopDrawable != null) {
            // leftTopDrawable.leftWidth
-            leftTopDrawable?.draw(batch, x, y + height - leftTopDrawable?.minHeight!!, leftTopDrawable?.minWidth!!, leftTopDrawable?.minHeight!!)
+            leftTopDrawable?.draw(batch, x, y + height - leftTopDrawable?.minHeight!!, min( leftTopDrawable?.minWidth!!, width/2), leftTopDrawable?.minHeight!!)
             if(rightTopDrawable != null){
-                rightTopDrawable?.draw(batch, x + width - rightTopDrawable?.minWidth!!, y + height - rightTopDrawable?.minHeight!!, rightTopDrawable?.minWidth!!, rightTopDrawable?.minHeight!!)
+                rightTopDrawable?.draw(batch, x + width - rightTopDrawable?.minWidth!!, y + height - rightTopDrawable?.minHeight!!, min(rightTopDrawable?.minWidth!!, width/2), rightTopDrawable?.minHeight!!)
 
             }
             else {
-                leftTopDrawable?.draw(batch, x + width, y + height - leftTopDrawable?.minHeight!!, -leftTopDrawable?.minWidth!!, leftTopDrawable?.minHeight!!)
+                leftTopDrawable?.draw(batch, x + width, y + height - leftTopDrawable?.minHeight!!, -min(leftTopDrawable?.minWidth!!, width/2), leftTopDrawable?.minHeight!!)
             }
 
 
@@ -100,7 +161,7 @@ class TilableFrame : Drawable{
                     batch,
                     x + leftTopDrawable?.minWidth!!,
                     y + height - leftTopHorizontalDrawable?.minHeight!!,
-                    leftTopHorizontalDrawable?.minWidth!!,
+                    min(leftTopHorizontalDrawable?.minWidth!!, width/2 - leftTopDrawable?.minWidth!!),
                     leftTopHorizontalDrawable?.minHeight!!
                 )
                 //leftTopHorizontalDrawable?.anchor = Anchor.RIGHT
@@ -108,11 +169,13 @@ class TilableFrame : Drawable{
                 if(rightTopHorizontalDrawable == null){
 
 
+                    // !! Note: negative width or height values works for mirroring only for DRAWABLE.
+                    //      For Tiled2DDrawables use positive values, but opposite anchoring!!
                     leftTopHorizontalDrawable?.draw(
                         batch,
                         x + width - leftTopDrawable?.minWidth!!,
                         y + height - leftTopHorizontalDrawable?.minHeight!!,
-                        -leftTopHorizontalDrawable?.minWidth!!,
+                        -min(leftTopHorizontalDrawable?.minWidth!!, width/2 - leftTopDrawable?.minWidth!!),
                         leftTopHorizontalDrawable?.minHeight!!
                     )
 
@@ -120,9 +183,57 @@ class TilableFrame : Drawable{
                 }
             }
 
+            if(leftTopVerticalDrawable != null){
+                /*
+                leftTopVerticalDrawable?.draw(
+                    batch,
+                    x + leftTopDrawable?.minWidth!!,
+                    y + height - leftTopVerticalDrawable?.minHeight!!,
+                    min(leftTopVerticalDrawable?.minWidth!!, width/2 - leftTopDrawable?.minWidth!!),
+                    leftTopVerticalDrawable?.minHeight!!
+                )
+
+                 */
+                leftTopVerticalDrawable?.draw(
+                    batch,
+                    x,
+                    y + height - leftTopDrawable?.minHeight!! -min(leftTopVerticalDrawable?.minHeight!!, height/2 - leftTopDrawable?.minHeight!!),
+                    leftTopVerticalDrawable?.minWidth!!,
+                    min(leftTopVerticalDrawable?.minHeight!!, height/2 - leftTopDrawable?.minHeight!!)
+                )
+                //leftTopHorizontalDrawable?.anchor = Anchor.RIGHT
+
+                if(rightTopVerticalDrawable == null){
+
+
+                    // !! Note: negative width or height values works for mirroring only for DRAWABLE.
+                    //      For Tiled2DDrawables use positive values, but opposite anchoring!!
+                    /*
+                    leftTopVerticalDrawable?.draw(
+                        batch,
+                        x + width - leftTopDrawable?.minWidth!!,
+                        y + height - leftTopVerticalDrawable?.minHeight!!,
+                        -min(leftTopVerticalDrawable?.minWidth!!, width/2 - leftTopDrawable?.minWidth!!),
+                        leftTopVerticalDrawable?.minHeight!!
+                    )
+
+                     */
+                    leftTopVerticalDrawable?.draw(
+                        batch,
+                        x + width,// - leftTopVerticalDrawable?.minWidth!!,
+                        y + height - leftTopDrawable?.minHeight!! - min(leftTopVerticalDrawable?.minHeight!!, height/2 - leftTopDrawable?.minHeight!!),
+                        -leftTopVerticalDrawable?.minWidth!!,
+                        min(leftTopVerticalDrawable?.minHeight!!, height/2 - leftTopDrawable?.minHeight!!)
+                    )
+
+
+                }
+            }
+
+
         }
 
-        if(topDrawable != null) {
+        if(tiledTopDrawable != null) {
             var spacerLeft = 0f
             if(leftTopDrawable != null)
                 spacerLeft += leftTopDrawable?.minWidth!!
@@ -131,12 +242,12 @@ class TilableFrame : Drawable{
 
             var spacerRight = spacerLeft
 
-            topDrawable?.draw(
+            tiledTopDrawable?.draw(
                 batch,
                 x + spacerLeft,
-                y + height - topDrawable?.minHeight!!,
-                width - spacerLeft - spacerRight,
-                topDrawable?.minHeight!!
+                y + height - tiledTopDrawable?.region?.regionHeight!!.toFloat(),
+                (width - spacerLeft - spacerRight),
+                tiledTopDrawable?.region?.regionHeight!!.toFloat()
             )
         }
 
--- core/src/com/unciv/ui/rendering/Tiled2DDrawable.kt
+++ core/src/com/unciv/ui/rendering/Tiled2DDrawable.kt
@@ -5,17 +5,22 @@ import com.badlogic.gdx.scenes.scene2d.utils.Drawable
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.Texture.TextureWrap
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.utils.ScissorStack
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable
 import com.badlogic.gdx.utils.JsonValue
+import com.unciv.UncivGame
+import com.unciv.ui.UncivStage
 import kotlin.math.sign
 
 enum class Tilability { PLANE, X, Y}
 enum class Anchor {CENTER, LEFT_TOP, TOP, RIGHT_TOP, RIGHT, RIGHT_BOTTOM, BOTTOM, LEFT_BOTTOM, LEFT}
 
+/** DEPRECATED! Use standart TiledDrawable class instead */
 class Tiled2DDrawable : Drawable {
     //private lateinit var texture: Texture
-    private lateinit var textureRegion: TextureRegion
+    lateinit var textureRegion: TextureRegion
     var anchor = Anchor.LEFT_TOP
     private var anchorString = ""
     private var tilability = Tilability.PLANE
@@ -124,6 +129,16 @@ class Tiled2DDrawable : Drawable {
         var i = 0
         var j = 0
 
+
+
+        val tiledDrawable = TiledDrawable(textureRegion)
+
+// Now you can use the TiledDrawable to draw
+// Assume batch is your SpriteBatch, and you want to fill an area at (50,50) with size (200,200)
+        ////batch.begin()
+        tiledDrawable.draw(batch, x,y,width,height)
+        return
+
         when (tilability) {
             Tilability.PLANE ->
                 while (drawX * sign(stepX.toFloat()) < finishX * sign(stepX.toFloat())) {
@@ -153,11 +168,68 @@ class Tiled2DDrawable : Drawable {
                 }
             Tilability.X ->
                 while (drawX * sign(stepX.toFloat()) < finishX * sign(stepX.toFloat())) {
+                    var currWidth = tileWidth
+                    var currHeight = tileHeight
+                    if((drawX + stepX) * sign(stepX.toFloat()) > finishX * sign(stepX.toFloat()))
+                        currWidth = (drawX + stepX) * sign(stepX.toFloat()) - finishX * sign(stepX.toFloat())
+
+               //     var textureRegion = textureRegion
+
+                    //val tiledDrawable = TiledDrawable(textureRegion)
+
+// Now you can use the TiledDrawable to draw
+// Assume batch is your SpriteBatch, and you want to fill an area at (50,50) with size (200,200)
+                    ////batch.begin()
+                    //tiledDrawable.draw(batch, x,y,width/2,height/2)
+                    //batch.end()
+
+                    // background?.draw(batch,x,y,width,height)
+
+                    // TODO: We need to fix overlapping of Tiled and non-tiled elements by cropping
+                    /*
+                    if(mirroredTiling)
+                        if(i % 2 == 0)
+                            drawWithCropping(batch,x + drawX, y + drawY, tileWidth, tileHeight)
+                        else
+                            drawWithCropping(batch,x + drawX + tileWidth, y + drawY, -tileWidth, tileHeight)
+
+
+                     */
+                    /*
                     if(mirroredTiling)
                         if(i % 2 == 0)
                             batch.draw(textureRegion, x + drawX, y + drawY, tileWidth, tileHeight)
                         else
                             batch.draw(textureRegion, x + drawX + tileWidth, y + drawY, -tileWidth, tileHeight)
+
+
+                     */
+                    /*
+                   if(mirroredTiling)
+                       if(i % 2 == 0)
+                           batch.draw(textureRegion, x + drawX, y + drawY, currWidth, tileHeight)
+                       else
+                           batch.draw(textureRegion, x + drawX + currWidth, y + drawY, -currWidth, tileHeight)
+
+
+                     */
+/*
+                    if (mirroredTiling)
+                        if (i % 2 == 0)
+                            tiledDrawable.draw(batch, x + drawX, y + drawY, currWidth, tileHeight)
+                        else
+                            tiledDrawable.draw(
+                                batch,
+                                x + drawX + currWidth,
+                                y + drawY,
+                                -currWidth,
+                                tileHeight
+                            )
+
+
+
+ */
+
 
                     drawX += stepX
                     i++
@@ -175,6 +247,28 @@ class Tiled2DDrawable : Drawable {
                 }
         }
     }
+
+
+    fun drawWithCropping(batch: Batch, x: Float, y: Float, width: Float, height: Float) {
+        // Define your area where rendering will be allowed.
+        val scissor = Rectangle()
+        val clipBounds = Rectangle(x, y, width, height)
+
+        scissor.set(x, y, width, height)
+        val camera = UncivGame.Current.screen?.stage?.camera
+        ScissorStack.calculateScissors(camera, batch.transformMatrix, clipBounds, scissor)
+
+        batch.flush() // Ensure batch is flushed before changing scissors.
+
+        // Now push the scissors to the ScissorStack which modifies the OpenGL scissor test.
+        if (ScissorStack.pushScissors(scissor)) {
+            //draw(batch,x, y, width, height)
+            batch.draw(textureRegion, x, y, width, height)
+            batch.flush() // Remember to flush the batch before popping scissors.
+            ScissorStack.popScissors()
+        }
+    }
+
 
     override fun getLeftWidth() = 0f
     override fun getRightWidth() = 0f
